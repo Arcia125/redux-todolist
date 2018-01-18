c77c896562ee44d38bdb2745eec0a663f6020440
Kevn Hallett <kevinmhallett@gmail.com>
2018-01-17 21:09:58 -0600

    add redux boilerplate

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux'
+
+const reducerExample = (state, action) => {
+  return state;
+};
+
+const reducers = combineReducers({
+  reducerExample
+});
+
+export default reducers;
