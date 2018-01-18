9be28236fb6c1dc1d6656d6ea67d9400af8e02a1
Kevn Hallett <kevinmhallett@gmail.com>
2018-01-17 20:33:10 -0600

    implement enzyme tests add snapshop test to App

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe('App', () => {
+  let component
+  it('renders without crashing', () => {
+    component = shallow(<App />)
+  });
+  it('matches our snapshot', () => {
+    expect(component).toMatchSnapshot()
+  })
+})
+
+
